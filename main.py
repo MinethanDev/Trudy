b0c29421466a89427c33d715995945a3be70fb1d
Ethan <125014979+MinethanDev@users.noreply.github.com>
2023-09-25 21:24:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ client = discord.Client(intents=discord.Intents.all())
 tree = app_commands.CommandTree(client)
 
 # Stockage release
-vm_chapitre = ""
-vm_date = ""
-ch_chapitre = ""
-ch_date = ""
+release_chap = {
+    'vm' : {'titre': "I became the villains mother", 'chapitre': "", 'date': "", 'image': "https://i.imgur.com/mLDfvZG.png  "},
+    'ch' : {'titre': "I am the child of this house", 'chapitre': "", 'date': "", 'image': "https://i.imgur.com/ji6brr1.png"}
+}
 
 # Initialisation du bot
 @bot.event
@@ -29,20 +29,10 @@ async def on_ready():
     app_commands.Choice(name="I am the child of this house", value="ch"),
     ])
 async def sortie(interaction: discord.Interaction, serie: app_commands.Choice[str], chapitre: str, date: str):
-    if (serie.value == 'vm'):
-        global vm_chapitre, vm_date
-        vm_chapitre = chapitre
-        vm_date = date
-
+        release_chap[serie.value]['chapitre'] = chapitre
+        release_chap[serie.value]['date'] = date
         print("{0} a programmé le chapitre {1} de la série {2} au {3}".format('<@' + str(interaction.user.id) + '>', chapitre, serie, date))
-        await interaction.response.send_message("{0}\nTu viens de programmer le chapitre **{1}** de la série **I became the villains mother** au **{2}** !".format('<@' + str(interaction.user.id) + '>', chapitre, date))
-        
-    elif (serie.value == 'ch'):
-        global ch_chapitre, ch_date
-        ch_chapitre = chapitre
-        ch_date = date
-        print("{0} a programmé le chapitre {1} de la série {2} au {3}".format('<@' + str(interaction.user.id) + '>', chapitre, serie, date))
-        await interaction.response.send_message("{0}\nTu viens de programmer le chapitre **{1}** de la série **I am the child of this house** au **{2}** !".format('<@' + str(interaction.user.id) + '>', chapitre, date))
+        await interaction.response.send_message("{0}\nTu viens de programmer le chapitre **{1}** de la série **{2}** au **{3}** !".format('<@' + str(interaction.user.id) + '>', chapitre, release_chap[serie.value]['titre'], date))
 
 # Commande /sortie
 @bot.tree.command(name="sortie", description="Savoir quand est prévue la prochaine sortie d'une série !")
@@ -51,10 +41,16 @@ async def sortie(interaction: discord.Interaction, serie: app_commands.Choice[st
     app_commands.Choice(name="I am the child of this house", value="ch"),
     ])
 async def sortie(interaction: discord.Interaction, serie: app_commands.Choice[str]):
-    if (serie.value == 'vm'):
-        await interaction.response.send_message("{0}\nLa prochaine sortie pour **I became the villains mother** est celle du chapitre **{1}** !\nElle est prévue au **{2}** !".format('<@' + str(interaction.user.id) + '>', vm_chapitre, vm_date))
-    elif (serie.value == 'ch'):
-        await interaction.response.send_message("{0}\nLa prochaine sortie pour **I am the child of this house** est celle du chapitre **{1}** !\nElle est prévue au **{2}** !".format('<@' + str(interaction.user.id) + '>', ch_chapitre, ch_date))
+        embed = discord.Embed(
+            title="Prochaine Sortie !",
+            description="Sortie du prochain chapitre de {0}".format(release_chap[serie.value]['titre']),
+            color=discord.Color.blue()
+        )
+        embed.add_field(name="Chapitre", value=release_chap[serie.value]['chapitre'], inline=False)
+        embed.add_field(name="Date de sortie", value=release_chap[serie.value]['date'], inline=False)
+        embed.set_author(name="Trudy", icon_url="https://i.imgur.com/X3LwsLZ.jpg")
+        embed.set_image(url=release_chap[serie.value]['image'])
+        await interaction.response.send_message('{0}'.format('<@' + str(interaction.user.id) + '>'), embed=embed)
 
 # Exécution du bot
 bot.run()
